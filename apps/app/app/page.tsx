2061bf5bd65a0df6bea49fcee21430cf66f3d908
BinZ <sikatunar@gmail.com>
2023-05-21 13:13:15 +0800

    test

--- apps/app/app/page.tsx
+++ apps/app/app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <ModalTutorialPage />
+      {/* <ModalTutorialPage /> */}
       <SwapPage />
     </>
   );
