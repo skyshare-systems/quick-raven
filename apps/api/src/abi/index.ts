969a7a8f1599a54d0832c0984b7c3e5fac101d31
_hvstle <hvstle.dev@gmail.com>
2023-06-05 06:55:58 +0800

    redo api

--- apps/api/src/abi/index.ts
+++
@@ -1,7 +0,0 @@
-import factory from "./factory.json";
-import LPToken from "./lpToken.json";
-import Token from "./token.json";
-
-export const lpTokenAbi = LPToken.abi;
-export const tokenAbi = Token.abi;
-export const factoryAbi = factory.abi;
--- apps/api/src/routes/factoryRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { GETpair } from "../controller/factoryController";
-
-const router = express.Router();
-
-router.get("/", GETpair);
-
-export default router;
