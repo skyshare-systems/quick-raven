cb39f68dbbe93527eb62c0257db979af32d75f69
BinZ <sikatunar@gmail.com>
2023-06-14 18:46:12 +0800

    Update WagmiProviders.tsx

--- apps/app/providers/WagmiProviders.tsx
+++ apps/app/providers/WagmiProviders.tsx
@@ -16,7 +16,10 @@ import React from "react";
 const { chains, publicClient } = configureChains(
   [polygonMumbai, bscTestnet, gnosisChiado],
   [
-    // alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+    alchemyProvider({
+      apiKey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImIyYTA3ZjJkLTAyNzMtNDdkOS04YjQzLWY4NDg5MTA5NWVkMCIsIm9yZ0lkIjoiMzM0NTE0IiwidXNlcklkIjoiMzQzOTQwIiwidHlwZUlkIjoiNDBiNzlkNGItYWQ1MC00NjQ2LThmZWQtYzk0YWJhY2JhNmFmIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE2ODQ0MDk1MjAsImV4cCI6NDg0MDE2OTUyMH0.ZpHJvU0X6g8hSbsEcVmwhtI39LSO7BKXyCqS8tIMn-0",
+    }),
     publicProvider(),
   ]
 );
