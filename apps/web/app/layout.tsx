6f7d8cb3a479be1ac5b65918bf74b127746bb4ac
Vince Irving Lucas <sikatunar@gmail.com>
2023-07-04 00:44:39 +0800

    Fix bug

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,4 @@
-import NavbarPage from "components/navigations/navbar";
 import "./globals.css";
-import FooterPage from "components/navigations/footer";
 
 export const metadata = {
   title: "Quick Raven",
