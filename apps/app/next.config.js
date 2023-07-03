93ab3539d1c7831e12ee7583bfb1ab4eed0d48e6
Vince Irving Lucas <sikatunar@gmail.com>
2023-07-04 02:15:54 +0800

    Bug test

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: false,
-  // transpilePackages: ["ui"],
+  reactStrictMode: false,
+  transpilePackages: ["ui"],
   experimental: {
     appDir: true,
   },
