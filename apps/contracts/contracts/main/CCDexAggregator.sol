de572e9afb7a56115e6f706132c8c2a022bec854
dev-hvstle <hvstle.dev@gmail.com>
2023-07-03 23:41:24 +0800

    CCAggregator upgrade

--- apps/contracts/contracts/main/CCDexAggregator.sol
+++ apps/contracts/contracts/main/CCDexAggregator.sol
@@ -10,12 +10,13 @@ import "@routerprotocol/evm-gateway-contracts/contracts/IGateway.sol";
 /// @title CCDexAggregator
 /// @author Skyshare Labs
 /// @notice A cross-chain ERC-20 smart contract to used to swap tokens
-/// on different chains using interoperable messagine.
+/// on different chains using interoperable messaging.
 contract CCDexAggregator is OwnableUpgradeable {
     // swap parameters struct where we specify which tokens should be swapped
     // on the other
     struct SwapParams {
         address dexRouter;
+        address destDexRouter;
         address tokenIn;
         address tokenOut;
         uint256 amountIn;
@@ -28,68 +29,109 @@ contract CCDexAggregator is OwnableUpgradeable {
     // chain type + chain id => address of our contract in bytes
     mapping(string => string) public contractList;
 
+    // address of Dex router => is enabled
+    mapping(address => bool) public dexRouters;
+
+    /**
+     * Initializer
+     */
+
     function initialize() external initializer {
         __Ownable_init();
         __Context_init();
     }
 
-    /// @notice function used when swapping tokens to DEXs
-    /// @param _dexRouter the address of the DEX router
-    /// @param _tokenIn address of token to be swapped
-    /// @param _tokenOut address of token to be received
-    /// @param _amountIn amount of tokens to swap
-    /// @param _amountOutMin minimum amount of tokens to receive
+    /**
+     * Modifiers
+     */
+    modifier bridgeConnect(
+        string memory _destChainId,
+        bytes memory _requestMetadata,
+        SwapParams memory _swapParams
+    ) {
+        _;
+        IGateway gateway = IGateway(gatewayContractAddress);
+
+        bytes memory packet = abi.encode(_swapParams);
+        bytes memory requestPacket = abi.encode(
+            contractList[_destChainId],
+            packet
+        );
+
+        gateway.iSend{value: msg.value}(
+            1,
+            0,
+            string(""),
+            _destChainId,
+            _requestMetadata,
+            requestPacket
+        );
+    }
+
+    /**
+     * Main Functions
+     */
+
     function swapToQr(
-        address _dexRouter,
-        address _tokenIn,
-        address _tokenOut,
-        uint256 _amountIn,
-        uint256 _amountOutMin
-    ) external {
-        IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
-        IERC20(_tokenIn).approve(_dexRouter, _amountIn);
+        string memory _destChainId,
+        bytes memory _requestMetadata,
+        SwapParams memory _swapParams
+    )
+        external
+        payable
+        bridgeConnect(_destChainId, _requestMetadata, _swapParams)
+    {
+        IERC20(_swapParams.tokenIn).transferFrom(
+            msg.sender,
+            address(this),
+            _swapParams.amountIn
+        );
+        IERC20(_swapParams.tokenIn).approve(
+            _swapParams.dexRouter,
+            _swapParams.amountIn
+        );
 
         address[] memory path = new address[](2);
-        path[0] = _tokenIn;
-        path[1] = _tokenOut;
+        path[0] = _swapParams.tokenIn;
+        path[1] = _swapParams.tokenOut;
 
-        IUniswapV2Router(_dexRouter).swapExactTokensForTokens(
-            _amountIn,
-            _amountOutMin,
+        IUniswapV2Router(_swapParams.dexRouter).swapExactTokensForTokens(
+            _swapParams.amountIn,
+            _swapParams.amountOutMin,
             path,
             address(this),
             block.timestamp
         );
     }
 
-    /// @notice function used when swapping tokens to users
-    /// @param _dexRouter the address of the DEX router
-    /// @param _tokenIn address of token to be swapped
-    /// @param _tokenOut address of token to be received
-    /// @param _amountIn amount of tokens to swap
-    /// @param _amountOutMin minimum amount of tokens to receive
-    function swapToUser(
-        address _dexRouter,
-        address _tokenIn,
-        address _tokenOut,
-        uint256 _amountIn,
-        uint256 _amountOutMin,
-        address _to
-    ) public {
-        IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
-        IERC20(_tokenIn).approve(_dexRouter, _amountIn);
+    function swapToUser(SwapParams memory _swapParams) public {
+        // IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
+        // IERC20(_tokenIn).approve(_dexRouter, _amountIn);
 
         address[] memory path = new address[](2);
-        path[0] = _tokenIn;
-        path[1] = _tokenOut;
+        path[0] = _swapParams.tokenIn;
+        path[1] = _swapParams.tokenOut;
 
-        IUniswapV2Router(_dexRouter).swapExactTokensForTokens(
-            _amountIn,
-            _amountOutMin,
+        IUniswapV2Router(_swapParams.destDexRouter).swapExactTokensForTokens(
+            _swapParams.amountIn,
+            _swapParams.amountOutMin,
             path,
-            _to,
+            _swapParams.to,
             block.timestamp
         );
+    }
+
+    function directToUser(
+        address _user,
+        address _token,
+        uint256 _amount
+    ) internal {
+        require(
+            msg.sender == gatewayContractAddress,
+            "QuickRaven :: Not gateway"
+        );
+        ERC20 token = ERC20(_token);
+        token.transfer(_user, _amount);
     }
 
     /// @notice function used to send cross chain messages
@@ -141,13 +183,10 @@ contract CCDexAggregator is OwnableUpgradeable {
         // decoding our payload
         SwapParams memory swapParams = abi.decode(packet, (SwapParams));
 
-        swapToUser(
-            swapParams.dexRouter,
-            swapParams.tokenIn,
+        directToUser(
+            swapParams.to,
             swapParams.tokenOut,
-            swapParams.amountIn,
-            swapParams.amountOutMin,
-            swapParams.to
+            swapParams.amountOutMin
         );
 
         return "";
@@ -163,5 +202,32 @@ contract CCDexAggregator is OwnableUpgradeable {
         IERC20 token = IERC20(_token);
 
         token.transfer(msg.sender, token.balanceOf(address(this)));
+    }
+
+    /**
+     * Setter Functions
+     */
+
+    function setGatewayContract(
+        address _gatewayContractAddress,
+        string memory _feePayerAddress
+    ) external onlyOwner {
+        gatewayContractAddress = _gatewayContractAddress;
+        IGateway(gatewayContractAddress).setDappMetadata(_feePayerAddress);
+    }
+
+    function setContractList(
+        string memory _chainId,
+        string memory _contract
+    ) external onlyOwner {
+        contractList[_chainId] = _contract;
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {
+        if (dexRouters[msg.sender]) {
+            return;
+        }
     }
 }
