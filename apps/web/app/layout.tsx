7854d86ed4cb936ecd4249b47dc72aedc3768e52
Vince Irving Lucas <sikatunar@gmail.com>
2023-07-04 17:17:23 +0800

    test

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, user-scalable=no" />
       </head>
       <body>
-        <ParallaxProvider>{children}</ParallaxProvider>
+        <ParallaxProvider scrollAxis="vertical">{children}</ParallaxProvider>
       </body>
     </html>
   );
